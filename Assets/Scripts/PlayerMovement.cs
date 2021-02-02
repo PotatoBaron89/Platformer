781891dfe1b8996afd2508fe27aeac6baf2aa082
Sam <samboy29@gmail.com>
2021-02-02 16:41:11 +1000

    added in jump and one way platforms

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ namespace Core {
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int _speed = 6;
+    [SerializeField] private int _jumpHeight = 200;
 
      void Update()
      {
@@ -14,6 +15,17 @@ public class PlayerMovement : MonoBehaviour
          var animator = GetComponent<Animator>();
          bool walking = horizontal != 0;
          animator.SetBool("Walk", walking);
+
+         if (horizontal != 0) 
+         { 
+             var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = horizontal < 0;
+         }
+
+         if (Input.GetButtonDown("Jump"))
+         {
+             rigidbody2D.AddForce(Vector2.up * _jumpHeight);
+         }
      }
     }
 }
