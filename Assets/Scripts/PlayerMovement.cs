92c8b54c7aaa05fe632a2e0d490b510c09c8c5a4
Sam <samboy29@gmail.com>
2021-02-02 15:49:15 +1000

    Added walk animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 namespace Core { 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int speed = 2;
+    [SerializeField] private int _speed = 6;
 
      void Update()
      {
-         var horizontal = Input.GetAxis("Horizontal") * speed;
+         var horizontal = Input.GetAxis("Horizontal") * _speed;
          var rigidbody2D = GetComponent<Rigidbody2D>();
          rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+
+         var animator = GetComponent<Animator>();
+         bool walking = horizontal != 0;
+         animator.SetBool("Walk", walking);
      }
     }
 }
