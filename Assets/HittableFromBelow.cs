c170d13c89e6a2cd0dd075f5d822d142367247c5
Sam <samboy29@gmail.com>
2021-02-13 14:00:33 +1000

    Used inheritance to animate our boxes

--- Assets/HittableFromBelow.cs
+++ Assets/HittableFromBelow.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class HittableFromBelow : MonoBehaviour
 {
     [SerializeField] protected Sprite _usedSprite;        // protected are private but can be used to inheritors
 
     protected virtual bool CanUse => true;
-    
+    private Animator _animator;
+    private static readonly int Use1 = Animator.StringToHash("Use");
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (CanUse == false) 
@@ -16,11 +24,18 @@ public class HittableFromBelow : MonoBehaviour
 
         if (other.contacts[0].normal.y > 0)
         {
+            PlayAnimation();
             Use();
             
             if (CanUse == false)
                 GetComponent<SpriteRenderer>().sprite = _usedSprite;
         }
+    }
+
+    private void PlayAnimation()
+    {
+        if (_animator != null)
+            _animator.SetTrigger(Use1);
     }
 
     protected virtual void Use()    // virtual allows us to override it
