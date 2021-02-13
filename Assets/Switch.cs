34b2b66b327583c2e54ffc3419a2e6260e6511d6
Sam <samboy29@gmail.com>
2021-02-14 00:51:41 +1000

    Improved functionality of the toggle swtich

--- Assets/Switch.cs
+++ Assets/Switch.cs
@@ -11,63 +11,33 @@ public class Switch : MonoBehaviour
     [SerializeField] Sprite _spriteRight;
     SpriteRenderer _spriteRenderer;
     private float distance;
-    private bool isLeft;
+    private bool isRight;
     private bool isTouching;
-
-    private Vector3 player = new Vector3();
-    private Vector3 self = new Vector3();
+    
     void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        self = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerStay2D(Collider2D other)
     {
+        var player = other.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        var playerRigidbody = player.GetComponent<Rigidbody2D>();
+        if (playerRigidbody == null)
+            return;
         
-       // if (Vector3.Distance(transform.position, gameObject<Player>.transform.position) < 1)
-        //    print("touching");
-    }
-
-    /*private void OnTriggerEnter2D(Collider2D other)
-    {
-        player = other.gameObject.transform.position;
-         
-        //Vector3.Distance(transform.position, player);
-
-        if (player.x < self.x && isLeft == false)
-        {
-            //_spriteRenderer.sprite = _spriteLeft;
-            isLeft = true;
-            print("Is left");
-        }
-        else if (player.x > self.x && isLeft == true)
-        {
-            //_spriteRenderer.sprite = _spriteRight;
-            isLeft = false;
-            print("is right");
-        }
-    }*/
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        player = other.gameObject.transform.position;
-        if (player.x < self.x && isLeft == false)
-        {
-            isLeft = true;
-            print("Is left");
-            if (_spriteLeft != null)
-                _spriteRenderer.sprite = _spriteLeft;
-        }
-        else if (player.x > self.x && isLeft == true)
-        {
-            if (_spriteRight != null)
-                _spriteRenderer.sprite = _spriteRight;
-            isLeft = false;
-            print("is right");
-        }
+        isRight = other.transform.position.x > transform.position.x;
+        bool playerWalkingRight = playerRigidbody.velocity.x > 0;
+        bool playerWalkingLeft = playerRigidbody.velocity.x < 0;
         
+        if (isRight && playerWalkingRight)
+            _spriteRenderer.sprite = _spriteRight;
+        else if (!isRight && playerWalkingLeft)
+            _spriteRenderer.sprite = _spriteLeft;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
